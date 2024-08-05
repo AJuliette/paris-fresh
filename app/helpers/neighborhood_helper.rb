1f12873272e5790838bfa58cb3b82feaf0d59074
Juliette Audema <juliette.audema@gmail.com>
2024-08-05 16:06:44 +0200

    Add emojis for types

--- app/helpers/neighborhood_helper.rb
+++ app/helpers/neighborhood_helper.rb
@@ -10,13 +10,13 @@ module NeighborhoodHelper
     when "district_town_hall" then "🏛️"
     when "boules_court" then ""
     when "place_of_worship" then "⛪"
-    when "permanent_shade_structure" then ""
+    when "permanent_shade_structure" then "🌳"
     when "swimming_pool" then "🏊‍♀️"
     when "library" then "📚"
-    when "public_baths" then ""
+    when "public_baths" then "🛀"
     when "museum" then "🖼️"
-    when "temporary_shade_structure" then ""
-    when "discovery_and_initiation" then ""
+    when "temporary_shade_structure" then "⛺"
+    when "discovery_and_initiation" then "🔍"
     end
   end
 end
