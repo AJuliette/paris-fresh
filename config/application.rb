c2b9c33afa3ec86eae78b89cbb2543eda55353ef
Juliette Audema <juliette.audema@gmail.com>
2024-08-05 02:08:09 +0200

    Add style and job scheduler

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module ParisFresh
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :sucker_punch
   end
 end
