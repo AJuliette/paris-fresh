a3bb663e79cee01e3a1dc9a577e1eb9722adde9e
Juliette Audema <juliette.audema@gmail.com>
2024-08-07 17:43:18 +0200

    Correct typo

--- app/models/fountain.rb
+++ app/models/fountain.rb
@@ -1,6 +1,6 @@
 class Fountain < ApplicationRecord
   scope :working, -> { where(working: true) }
-  scope :with_addresss, -> { where.not(street_number: nil) }
+  scope :with_address, -> { where.not(street_number: nil) }
 
   belongs_to :neighborhood
 end
