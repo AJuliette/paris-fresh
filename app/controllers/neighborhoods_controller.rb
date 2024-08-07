64185782e412e4370f4ce5ec3f93c74d803814e6
Juliette Audema <juliette.audema@gmail.com>
2024-08-07 22:42:57 +0200

    Improve index page

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -4,7 +4,7 @@ class NeighborhoodsController < ApplicationController
   end
 
   def show
-    @neighborhood = Neighborhood.find_by_number(params[:number])
+    @neighborhood = Neighborhood.find_by_number(params[:number]) || Neighborhood.find(params[:number])
     @fresh_places = @neighborhood.fresh_places.order(:place_type)
     @fountains = Fountain.working.with_address.where(neighborhood: @neighborhood)
     @green_spaces = @neighborhood.green_spaces
