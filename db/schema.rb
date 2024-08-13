c82bc2beff1b0085a4c6e9bbe0315a72e924646b
Juliette Audema <juliette.audema@gmail.com>
2024-08-13 08:57:28 +0200

    Update to Rails 7.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_07_173540) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_13_065346) do
   create_table "fountains", force: :cascade do |t|
     t.integer "open_data_identifier"
     t.boolean "working"
